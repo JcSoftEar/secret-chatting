1107371f72035413aaa08d00283f99ccf880a635
应晔 <james@taogame.com>
2026-05-12 17:25:26 +0800

    加入docker

--- app.py
+++ app.py
@@ -264,4 +264,4 @@ def handle_admin_join(data):
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-this-in-production'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///secret_chatting.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///instance/secret_chatting.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
